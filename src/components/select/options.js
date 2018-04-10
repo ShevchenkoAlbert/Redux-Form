1b3e3d6e5c79de38af4dac05cbbc7ad45530688a
Albert Shevchenko <shevchenkoalbert@shevchenkoalbert-lx.nixsolutions.com>
2018-04-10 09:16:22 +0300

    select

---
+++ src/components/select/options.js
@@ -0,0 +1,56 @@
+const options = {
+  contacts: [
+    { 
+	    label: 'Phone',
+	    value: 'Phone',
+    },
+    { 
+	    label: 'Skype',
+	    value: 'Skype',
+    },
+    { 
+	    label: 'Telegram',
+	    value: 'Telegram',
+    },
+    { 
+	    label: 'Facebook',
+	    value: 'Facebook',
+    },
+    { 
+	    label: 'Viber',
+	    value: 'Viber',
+    },
+    { 
+	    label: 'Twitter',
+	    value: 'Twitter',
+    },    
+  ],
+  hobbies: [
+    {
+      label: 'Photos',
+	    value: 'Photos',
+    },
+    {
+      label: 'Auto',
+	    value: 'Auto',
+    },
+    {
+      label: 'Moto',
+	    value: 'Moto',
+    },
+    {
+      label: 'Books',
+	    value: 'Books',
+    },
+    {
+      label: 'Programming',
+	    value: 'Programming',
+    },
+    {
+      label: 'Animals',
+	    value: 'Animals',
+    },
+  ]
+}
+
+export default options;
